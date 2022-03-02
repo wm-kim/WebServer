f21468d5a40f779da00ee5847e0116e78df5d82a
feudalsociety <32698816+feudalsociety@users.noreply.github.com>
2022-03-02 14:56:50 +0900

    WebAPI

---
+++ HelloAspNet/WebEmpty/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using HelloEmpty.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloEmpty.Controllers
+{
+    // 라우팅 자체를 여기서 따로 따로 설정함.
+    // 지금껏 홈페이지에서 경로 설정하고 어떤 페이지를 보여줄지 정하고 있었음
+    // Razor의 경우 Pages 폴더 산하에서 페이지를 검색함.
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // get put post 다양한 방식이 있음
+        [HttpGet]
+        public List<HelloMessage> Get()
+        {
+            List<HelloMessage> messages = new List<HelloMessage>();
+            messages.Add(new HelloMessage() { Message = "Hello WEP API 1" });
+            messages.Add(new HelloMessage() { Message = "Hello WEP API 2" });
+            messages.Add(new HelloMessage() { Message = "Hello WEP API 3" });
+            return messages; // MVC에서는 view를 반환했음
+        }
+    }
+}
--- HelloAspNet/WebEmpty/Program.cs
+++ HelloAspNet/WebEmpty/Program.cs
@@ -1,27 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container. 
-builder.Services.AddRazorPages();
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
 app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
 
 app.UseAuthorization();
 
-// mvc처럼 경로를 설정해주지 않았지만 기본적응로 아무것도 입력하지 않았을때는
-// Pages 아래 있는걸 알아서 찾아줌
-app.MapRazorPages();
+app.MapControllers();
 
 app.Run();
