ece10b21de83a31340cd88e92e7e8af422f31ce8
feudalsociety <32698816+feudalsociety@users.noreply.github.com>
2022-03-03 21:29:46 +0900

    form validation

--- BlazorApp/BlazorStudy/Data/WeatherForecastService.cs
+++ BlazorApp/BlazorStudy/Data/WeatherForecastService.cs
@@ -8,14 +8,15 @@ namespace BlazorStudy.Data
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-        public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
+        public Task<List<WeatherForecast>> GetForecastAsync(DateTime startDate)
         {
             return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = startDate.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            }).ToArray());
+            }).ToList());
         }
     }
 }
+
