cfd6d3634730c7c471294c973a22210200638df5
feudalsociety <32698816+feudalsociety@users.noreply.github.com>
2022-03-03 23:30:17 +0900

    state 관리

---
+++ BlazorApp/BlazorApp1/Data/CounterState.cs
@@ -0,0 +1,31 @@
+﻿namespace BlazorApp1.Data
+{
+	// 논리적으로 보면 CounterState라는 서비스를 만들어서 관리하면 전역에서 사용하는 느낌으로 할 수 있겠다.
+	// Program쪽에 Singleton/Transient/Scoped중 택1, 이 경우는 Scoped를 해야 유저별로 관리가 될거임
+	public class CounterState
+	{
+		int _count = 0;
+		// public int Count { get; set; }
+
+		public Action OnStateChanged;
+
+		public int Count
+		{ 
+			get
+			{
+				return _count;
+			}
+
+			set
+			{
+				_count = value;
+				Refresh();
+			}
+		}
+
+		void Refresh()
+		{
+			OnStateChanged?.Invoke();
+		}
+	}
+}
--- BlazorApp/BlazorStudy/Data/WeatherForecast.cs
+++ BlazorApp/BlazorStudy/Data/WeatherForecast.cs
@@ -1,13 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BlazorStudy.Data
 {
     public class WeatherForecast
     {
         public DateTime Date { get; set; }
 
+        // 정말 중요하고 필수적이라면. 범위도 제한 가능
+        [Required(ErrorMessage = "Need TemperatureC")]
+        [Range(typeof(int), "-100", "100")]
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
+        
+        [Required(ErrorMessage = "No Summary!")]
+        [StringLength(10, MinimumLength =2, ErrorMessage ="2~10")]
         public string? Summary { get; set; }
     }
 }
